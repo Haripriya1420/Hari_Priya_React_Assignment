dd18e48b6df5d4f86bb51ee1c3359cbccd6862a5
Hari Priya S <haripriya200041@gmail.com>
2025-02-05 14:25:39 +0530

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App"; // Ensure you have an App.js file
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
